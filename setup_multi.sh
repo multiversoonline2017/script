8529fd8e457e091ecc3dcdae4d964e8f0c52d905
multiversoonline2017 <47371998+multiversoonline2017@users.noreply.github.com>
2019-10-31 22:08:56 -0300

    Update setup_multi.sh

--- setup_multi.sh
+++ setup_multi.sh
@@ -32,11 +32,13 @@ if [[ $DOSETUP =~ "y" ]] ; then
   cd /var
   sudo touch swap.img
   sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=5120
   sudo mkswap /var/swap.img
   sudo swapon /var/swap.img
   sudo free
   sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+  sudo sysctl vm.swappiness=50
+  echo “vm.swappiness = 50” >> /etc/sysctl.conf 
   cd
 
   ## INSTALL
