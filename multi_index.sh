d63b558f18d4236812298994d7b7a677b8a9a390
multiversoonline2017 <47371998+multiversoonline2017@users.noreply.github.com>
2020-06-04 16:31:30 -0300

    Update multi_index.sh

--- multi_index.sh
+++ multi_index.sh
@@ -29,13 +29,31 @@ if [[ $DOSETUP =~ "y" ]] ; then
   sudo apt-get install -y automake unzip
   sudo apt-get update
   sudo apt update && sudo apt upgrade -y
+
+  cd /var
+  sudo touch swap.img
+  sudo chmod 600 swap.img
+  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=5120
+  sudo mkswap /var/swap.img
+  sudo swapon /var/swap.img
+  sudo free
+  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+  sudo sysctl vm.swappiness=50
+  echo “vm.swappiness = 50” >> /etc/sysctl.conf 
+  cd
   
 
   ## INSTALL
   wget https://github.com/IndexChain/Index/releases/download/v0.13.10.5/index-0.13.10.5-x86_64-linux-gnu.tar.gz
   sudo apt-get install unzip
   sudo apt-get install tar
-  sudo tar xzvf index-0.13.10.5-x86_64-linux-gnu.tar.gz 
+  sudo tar xzvf index-0.13.10.5-x86_64-linux-gnu.tar.gz -d /usr/local/bin
+  chmod +x /usr/local/bin/indexd
+  chmod +x /usr/local/bin/index-cli
+  chmod +x /usr/local/bin/index-qt
+  sudo chmod 755 IndexChain*
+  sudo mv IndexChain* /usr/bin
+  cd
   rm -rf index-0.13.10.5-x86_64-linux-gnu.tar.gz
 
   sudo apt-get install -y ufw
@@ -52,7 +70,7 @@ if [[ $DOSETUP =~ "y" ]] ; then
 fi
 
 ## Setup conf
-mkdir -p ~/index-0.13.10.5/bin
+mkdir -p ~/bin
 IP=$(curl -s4 http://ip.42.pl/raw)
 NAME="index"
 CONF_FILE=index.conf
@@ -86,18 +104,18 @@ for i in `seq 1 1 $MNCOUNT`; do
   CONF_DIR=~/.${NAME}_$ALIAS
 
   # Create scripts
-  echo '#!/index-0.13.10.5/bin/bash' > ~/index-0.13.10.5/bin/${NAME}d_$ALIAS.sh
-  echo "${NAME}d -daemon -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/index-0.13.10.5/bin/${NAME}d_$ALIAS.sh
-  echo '#!/index-0.13.10.5/bin/bash' > ~/index-0.13.10.5/bin/${NAME}-cli_$ALIAS.sh
-  echo "${NAME}-cli -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/index-0.13.10.5/bin/${NAME}-cli_$ALIAS.sh
-  chmod 755 ~/index-0.13.10.5/bin/${NAME}*.sh
+  echo '#!/bin/bash' > ~/bin/${NAME}d_$ALIAS.sh
+  echo "${NAME}d -daemon -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~bin/${NAME}d_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/${NAME}-cli_$ALIAS.sh
+  echo "${NAME}-cli -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/bin/${NAME}-cli_$ALIAS.sh
+  chmod 755 ~/bin/${NAME}*.sh
 
   mkdir -p $CONF_DIR
   echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
   echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
   echo "rpcallowip=127.0.0.1" >> ${NAME}.conf_TEMP
   echo "rpcport=$RPCPORT" >> ${NAME}.conf_TEMP
-  echo "-listen=0" >> ${NAME}.conf_TEMP
+  echo "listen=0" >> ${NAME}.conf_TEMP
   echo "server=1" >> ${NAME}.conf_TEMP
   echo "daemon=1" >> ${NAME}.conf_TEMP
   echo "logtimestamps=1" >> ${NAME}.conf_TEMP
@@ -108,10 +126,16 @@ for i in `seq 1 1 $MNCOUNT`; do
   echo "bind=[$IPADDRESS]:7082" >> ${NAME}.conf_TEMP
   echo "indexnode=1" >> ${NAME}.conf_TEMP
   echo "indexnodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
+  echo "addnode=45.76.196.198:7082" >> ${NAME}.conf_TEMP
+  echo "addnode=5.3.65.34:7082" >> ${NAME}.conf_TEMP
+  echo "addnode=167.86.108.169:7082" >> ${NAME}.conf_TEMP
+  echo "addnode=173.212.227.202:7082" >> ${NAME}.conf_TEM
+  echo "addnode=45.76.56.185:7082" >> ${NAME}.conf_TEM
   
   sudo ufw allow [$IPADDRESS]:7082/tcp
 
   mv ${NAME}.conf_TEMP $CONF_DIR/${NAME}.conf
   
-  sh ~/index-0.13.10.5/bin/${NAME}d_$ALIAS.sh
+  sh ~/bin/${NAME}d_$ALIAS.sh
 done
